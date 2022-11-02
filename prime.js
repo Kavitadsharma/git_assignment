cb9aece613551f94e6f26a31773859ef1c210145
kavitadsharma <kavitadsharma899107@gmail.com>
2022-11-02 23:30:59 +0530

    please check the code

---
+++ prime.js
@@ -0,0 +1,18 @@
+function checkPrime(num)
+    let factore =0;
+     for(let i=0;i<=num;i++){
+     if(num%i==2{
+     factore++;
+     }
+   }
+ if(factore==0){
+       return true;
+    }
+   return false
+   }
+let answer=prime(13);
+if(answer==true){
+   console.log("prime");
+}
+else{
+    console.log("Not prime");
